eb3f83e5fe23fd0377f5418f306872cc751b7702
MikkosaurusRex <jaismikko@gmail.com>
2026-01-18 16:53:28 +0100

    Update folder_classify.dart

--- lib/folder_classify.dart
+++ lib/folder_classify.dart
@@ -6,7 +6,7 @@ import 'package:gpth/utils.dart';
 import 'package:path/path.dart' as p;
 
 bool isYearFolder(Directory dir) =>
-    RegExp(r'^Photos from (20|19|18)\d{2}$').hasMatch(p.basename(dir.path));
+    RegExp(r'^Foto_s van (20|19|18)\d{2}$').hasMatch(p.basename(dir.path));
 
 Future<bool> isAlbumFolder(Directory dir) =>
     dir.parent.list().whereType<Directory>().any((e) => isYearFolder(e));
